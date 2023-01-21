84753be47a16a6f6a3ccd1f0dc7b3f2546b75343
Aditya Kumar <106439535+AddySoulja@users.noreply.github.com>
2023-01-21 18:04:31 +0000

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,29 @@
-import React , {useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import '../styles/App.css';
 import List from "./List";
 const App = () => {
   //code here
+  const [inputText, setInputText] = useState('');
+  const [list, setList] = useState([]);
+
+  const addInList = () => {
+    setList([...list, inputText]);
+  }
+
+  useEffect(() => {
+    setInputText('');
+  }, [list])
+
   return (
     <div id="main">
-       <input id="input" onChange={} />
-      <button id="button" onClick={}>Click</button>
-       <ul id="list">
-      <List listx={list} />
+      <input
+        id="input"
+        onChange={(e) => setInputText(e.target.value)}
+        value={inputText}
+      />
+      <button id="button" onClick={addInList}>Click</button>
+      <ul id="list">
+        <List listx={list} />
       </ul>
     </div>
   )
--- src/components/List.js
+++ src/components/List.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ListItems from './ListItems';
 //code here
- const List = ({listx}) =>{
-  return(
-    
- 
-    <ListItems valuex={i} />
-    
-  
+const List = ({ listx }) => {
+  return (
+    <>
+      <div id='list'>
+        {
+          listx.map((i, idx) => <ListItems valuex={i} index={idx} />)
+        }
+      </div>
+    </>
   )
 }
 
